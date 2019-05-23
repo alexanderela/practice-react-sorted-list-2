fa64e916eee71ad9e63047985addab125cb5bd13
Alexander Ela <ela.alexander@gmail.com>
2019-05-22 21:48:29 -0600

    Create NewTaskForm and TaskContainer files

---
+++ src/NewTaskForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './NewTaskForm.css';
+
+class NewTaskForm extends Component {
+	constructor() {
+		super();
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+			<form className='NewTaskForm'>
+			
+			</form>
+		)
+	}
+}
+
+export default NewTaskForm;
